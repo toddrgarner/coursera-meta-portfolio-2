f0e33dfcf82c17aef21606db6bc9a2c2aeafcb9e
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2023-10-26 17:25:02 -0700

    modified Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
